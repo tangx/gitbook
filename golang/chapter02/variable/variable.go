a73e18f04cca21e6d48abe58fffd000cec76272b
tangx <uyinn@live.com>
2023-02-27 06:37:53 +0000

    Update from https://github.com/tangx/gitbooks-golang/commit/63a8072a98787b79f7bb2ff70e6732f7990eb939

---
+++ golang/chapter02/variable/variable.go
@@ -0,0 +1,40 @@
+package chapter02
+
+import "fmt"
+
+const (
+	Monday   = 0
+	Tuesday  = iota // 2
+	_               // 3。 周 3 跳过, 但使用站位符是 iota 递增
+	Thursday = iota // 4
+	// Friday
+	// Saturday
+	Sunday = iota + 2 // 7。 5和6不存在， 因此 Sunday 是 iota+2
+)
+
+var abc int8 = 100 // 指定 abc 的类型是 int8
+var def = 100      // 推定类型为 int
+
+func demo() {
+	a1 := 100
+	var a2 = 100
+	fmt.Println(a1, a2)
+}
+
+func demo2(gg int) {
+	fmt.Println(gg)
+}
+
+func assignment() {
+	abc := "word"
+	abc = 123 // cannot use 123 (untyped int constant) as string value
+
+	fmt.Println(abc)
+}
+
+func assignmentNumber() {
+	var n int = 100
+	var n8 int8
+	n8 = n // cannot use n (variable of type int) as int8 value
+	fmt.Println(n8)
+}
