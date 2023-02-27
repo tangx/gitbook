1516699fe80cfa84696a7de786603452d8566411
tangx <uyinn@live.com>
2023-02-27 08:07:34 +0000

    Update from https://github.com/tangx/gitbooks-golang/commit/784d7cab7695cb541cf746fe20bd58b89f205f91

--- golang/chapter02/process-control/for-loops/for-loops.go
+++ golang/chapter02/process-control/for-loops/for-loops.go
@@ -39,3 +39,20 @@ func deadLoop() {
 		time.Sleep(1 * time.Second)
 	}
 }
+
+func breakLoops() {
+	for i := 1; i <= 10; i++ {
+
+		if i%2 == 0 {
+			// 当 i 是双数的时候， 退出当前循环
+			continue
+		}
+
+		if i > 6 {
+			// 当 i 大于 6 的时候， 结束所有循环
+			break
+		}
+
+		fmt.Println(i)
+	}
+}
